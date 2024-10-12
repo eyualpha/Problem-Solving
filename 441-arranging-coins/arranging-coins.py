c4ebe809b1448e70d64a665f887f93a5aa895a24
Eyu Ashenafi <143643131+eyualpha@users.noreply.github.com>
2024-10-12 16:24:21 +0300

    Time: 42 ms (63.18%) | Memory: 16.5 MB (70.07%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,9 +1,15 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        counter = 0
-        i = 1
-        while n>=i:
-            n -= i
-            i += 1
-            counter += 1
-        return counter
+        left, right = 0, n
+
+        while left<=right:
+            mid = (left+right)//2
+            equ = mid*(mid+1)//2
+            if equ == n:
+                return mid
+            elif n > equ:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
+            
