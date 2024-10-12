fd438ff9e2b126e3519d316ac136c9725d7c7862
Eyu Ashenafi <143643131+eyualpha@users.noreply.github.com>
2024-10-12 10:15:07 +0300

    Time: 1689 ms (5.01%) | Memory: 16.4 MB (93.90%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        for i in range(x+1):
+            if i*i == x:
+                return i
+            if i*i > x:
+                return i - 1
+        if x == 0:
+            return 0
+            
+
+        
