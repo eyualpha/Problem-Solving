de9e56c742620ba360847a08e1494273871beda2
Eyu Ashenafi <143643131+eyualpha@users.noreply.github.com>
2024-10-28 11:50:23 +0300

    Time: 28 ms (21.95%) | Memory: 16.8 MB (38.49%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,14 +1,25 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         store = {}
+        note = {}
         for i in magazine:
             if i not in store:
                 store[i] = 1
             else:
                 store[i] += 1
+        # for j in ransomNote:
+        #     if j not in note:
+        #         note[j] = 1
+        #     else:
+        #         note[j] += 1
+        # for k in ransomNote:
+        #     if k not in store or note[k] > store[k]:
+        #         return False
+        # return True
         for j in ransomNote:
             if j not in store or store[j] == 0:
                 return False
             else:
                 store[j] -= 1
         return True
+
